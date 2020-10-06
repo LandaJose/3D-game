6639a058e4e2512d3ee19e6859b2627208caf61e
David Hermosillo <70419213+DavidHermosillo@users.noreply.github.com>
2020-10-05 22:51:31 -0700

    updated scripts and assets

--- Assets/ChasePlayer.cs
+++ Assets/ChasePlayer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class ChasePlayer : MonoBehaviour
 {
     private NavMeshAgent crawler;
     public GameObject Player;
     public float crawlerDistanceRun = 5.0f;
+    private bool beginCrawling = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +20,25 @@ public class ChasePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var keyboard = Keyboard.current;
+
+        if (keyboard != null)
+        {
+            if (keyboard.tabKey.wasPressedThisFrame && !beginCrawling)
+            {
+                beginCrawling = true;
+            }
+            else if (keyboard.tabKey.wasPressedThisFrame && beginCrawling)
+            {
+                beginCrawling = false;
+            }
+        }
         float distance = Vector3.Distance(transform.position, Player.transform.position);
-        if (distance < crawlerDistanceRun)
+        if (distance < crawlerDistanceRun && beginCrawling)
         {
             Vector3 dirToPlayer = transform.position - Player.transform.position;
             Vector3 newPosition = transform.position - dirToPlayer;
+
             crawler.SetDestination(newPosition);
         }
     }
