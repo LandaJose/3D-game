99a4b839df0ec13187bade5f52f3ca0453b553bd
LandaJose <joselanda978@gmail.com>
2020-10-06 19:03:56 -0700

    Revert "Merge pull request #3 from LandaJose/assetsAnimationsUpdatedChase"
    This reverts commit 66e4902edf20bf0b9e1d1b08e19fc09b54167a98, reversing
    changes made to 98fef44f365285194074cf7407d7a57ac8f4cd38.

--- Assets/ViewController.cs
+++ Assets/ViewController.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class ViewController : MonoBehaviour
 {
     public float mouseSensitivity = 20.0f;
-    public float xRotation = 1f;
+    public float xRotation = 0f;
     public Transform playerBody;
     void Start()
     {
@@ -23,7 +23,7 @@ public class ViewController : MonoBehaviour
             float mouseY = mouse.delta.y.ReadValue() * mouseSensitivity * Time.deltaTime;
             playerBody.Rotate(Vector3.up * mouseX);
             xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -80f, 45f);
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
             transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         }
 
