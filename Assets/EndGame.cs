2e9cb9381189c9dc6b9ac73b0dc211109b12aafb
David Hermosillo <70419213+DavidHermosillo@users.noreply.github.com>
2020-11-05 16:18:38 -0800

    crawler will now cause a game over and restart fuctionality is now available

---
+++ Assets/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public static Boolean gameEnded = false;
+    public void EndedGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Restart();
+        }
+    }
+
+    private void Restart()
+    {
+        FindObjectOfType<GameOverMenu>().SetBeginRestart();
+    }
+}
