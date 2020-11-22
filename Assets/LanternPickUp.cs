fbf53d0fe93f55c5fbd355a847660ff293310888
Kgiang6 <70337556+Kgiang6@users.noreply.github.com>
2020-11-21 18:15:53 -0800

    Added footsteps and background music

---
+++ Assets/LanternPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class LanternPickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioS;
+    void Start()
+    {
+        audioS = gameObject.GetComponent<AudioSource>();
+
+    }
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        audioS.PlayOneShot(audioS.clip);
+    }
+
+}
