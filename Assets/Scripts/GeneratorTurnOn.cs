871660a524ba89683c1290ec19d5b384e05d05fa
Kgiang6 <70337556+Kgiang6@users.noreply.github.com>
2020-11-26 18:19:15 -0800

    Pick up audio/Moved scripts & audio in folders

---
+++ Assets/Scripts/GeneratorTurnOn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorTurnOn : MonoBehaviour
+{
+    public AudioClip[] GeneratorAudio;
+    public AudioSource audioS;
+
+    public void PlayGeneratorAudio()
+    {
+        audioS.PlayOneShot(GeneratorAudio[0]);
+    }
+}
