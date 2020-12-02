a2cc0110c78134cd7fb7d5ddc4ba981f58eccea6
David Hermosillo <70419213+DavidHermosillo@users.noreply.github.com>
2020-12-01 16:58:09 -0800

    added the third puzzle and game win screen (#31)

---
+++ Assets/WinGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    public static bool gameWon = false;
+    public GameObject gameWonUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameWon)
+        {
+            DisplayMenu();
+        }
+    }
+
+    void DisplayMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        gameWonUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        EndGame.gameEnded = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        gameWonUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameWon = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
